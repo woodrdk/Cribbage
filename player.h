7f1477932589916b15b24ea5e692a85b958bb0ec
Robert Wood Jr <rdrwood@gmail.com>
2021-01-23 22:05:08 -0800

    player .h

--- player.h
+++ player.h
@@ -1,5 +1,20 @@
 #pragma once
-class player
-{
-};
 
+#include <iostream>
+using namespace std;
+
+class Player
+{
+private:
+	string name;
+	int score;
+	int crib;
+
+public:
+	string toString();
+	void setName(string newName);
+	string getName();
+	void setScore(int score);
+	int seeScore();
+	
+};
